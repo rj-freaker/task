41cf45cbe014c0163f097f08ec7ad1545e71f466
rj-freaker <68267959+rj-freaker@users.noreply.github.com>
2023-10-12 23:58:07 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='text-center'>
+        <div>
+            <h1 className='font-weight-bold display-4'>
+              Welcome to the Home Page
+            </h1>
+            <p className='lead'>
+              This is simple website which is responsive.
+            </p>
+        </div>
+    </div>
+  )
+}
